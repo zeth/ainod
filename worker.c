eb768ba9d74e6d9341144a0bd98123df273ba1dc
Zeth <theology@gmail.com>
2015-06-03 22:05:40 +0100

    Add little message back to client.

--- worker.c
+++ worker.c
@@ -13,40 +13,35 @@
 #include "worker.h"
 #include "mutex.h"
 
+#include <string.h>
+
+
 #define BACKLOG 10
 #define MAX_EPOLL_FD 4096
 #define MAXEVENTS 64
 #define MAX_BUF 1000
-char buf[MAX_BUF];
+
+static void setnonblocking(int fd)
+{
+  int opts;
+  opts = fcntl(fd, F_GETFL);
+  if (opts < 0) {
+    handle_error("fcntl failed\n");
+    return;
+  }
+  opts = opts | O_NONBLOCK;
+  if (fcntl(fd, F_SETFL, opts) < 0) {
+    handle_error("fcntl failed\n");
+    return;
+  }
+  return;
+}
 
 /* child_worker is the program that the children each run.
 
    The child process learns its own worker number through the worker
    argument.
 */
-
-static int make_socket_non_blocking (int sfd)
-{
-  int flags, s;
-
-  flags = fcntl (sfd, F_GETFL, 0);
-  if (flags == -1)
-    {
-      perror ("fcntl");
-      return -1;
-    }
-
-  flags |= O_NONBLOCK;
-  s = fcntl (sfd, F_SETFL, flags);
-  if (s == -1)
-    {
-      handle_error("fcntl");
-      return -1;
-    }
-
-  return 0;
-}
-
 
 void child_worker(int worker,
                   pthread_mutex_t *mp,
@@ -64,10 +59,10 @@ void child_worker(int worker,
     handle_error("Worker could not create epoll.");
   }
 
+  /*Should the event object be on the heap? */
   struct epoll_event event;
   event.data.fd = incoming;
   event.events = EPOLLIN | EPOLLET;
-  /* event.events = EPOLLIN; */
   if (epoll_ctl (epoll_fd, EPOLL_CTL_ADD, incoming, &event) == -1) {
     handle_error("Could not add socket to epoll.");
   }
@@ -79,25 +74,41 @@ void child_worker(int worker,
     /* printf("CHILD %d, try to get mutex\n", worker); */
     int first = lock_mutex(mp);
     if (first == 0) {
-      printf("CHILD %d, I won the mutex! %d events \n", worker, number_of_events);
+      /* printf("CHILD %d, I won the mutex! %d events \n", worker, number_of_events); */
       int i;
       for (i = 0; i < number_of_events; i++) {
         /* What to do here now ? */
         if (events[i].events & EPOLLIN) {
-          printf("i is %d\n", i);
-          /* Accept the connection */
+          /* Connection file descriptor */
           int cfd;
+          /* Accept the connection */
           cfd = accept(events[i].data.fd, NULL, NULL);
-          printf("cfd is %d", cfd);
           if (cfd == -1) {
-            handle_error("Error in accepting socket");
+            handle_error("Error in accepting connection");
           }
+          char buf[100];
+          ssize_t numRead;
+          char *response = "Hello\n";
+          int response_success = send(cfd,
+                                      response,
+                                      sizeof(response)+1,
+                                      MSG_DONTWAIT);
+          if (response_success == -1) {
+            handle_error("Could not send to client socket.");
+          }
+          /* It is the while loop here that sucks. */
+          while ((numRead = read(cfd, buf, 1000)) > 0) {
+            if (write(STDOUT_FILENO, buf, numRead) != numRead) {
+              handle_error("partial/failed write");
+            }
+          }
+          close(cfd);
         } else {
           printf("Some other kind of event.");
         }
-        
+
       }
-      sleep(5);
+
       int second = unlock_mutex(mp);
 
     } else {
