f44c2837948ffcf10dda489faf48f83682e30da4
Zeth <theology@gmail.com>
2015-11-05 14:50:03 +0000

    Made sure licence is in all C files.

--- src/jsonrpc.c
+++ src/jsonrpc.c
@@ -1,3 +1,22 @@
+/***
+  This file is part of ainod.
+
+  Copyright 2015 Zeth
+
+  ainod is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  ainod is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with ainod; If not, see <http://www.gnu.org/licenses/>.
+***/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
